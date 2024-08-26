5659ed87da35219ac3a1ebcc11f0eb9e2d35abe1
Ilhom <khaitovilhom04@gmail.com>
2024-08-26 17:54:26 +0500

    next js

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -1,11 +1,14 @@
 "use client"
 
-
 import ProductDetails from "@/src/components/product-details/ProductDetails";
 import fetchData from "@/src/utils/fetchData";
+import notFound from "@/app/not-found";
 
 const Page = async ({params}) => {
   const product = await fetchData(`https://dummyjson.com/products/${params.id}`);
+  if (!product) {
+    notFound()
+  }
   return (
       <div className="max-w-[1000px] mx-auto flex justify-center items-center">
         <ProductDetails product={product}/>
--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -1,6 +1,7 @@
 const FetchData = async (url) => {
   try {
     const response = await fetch(url);
+    if(!response.ok) return undefined
     return await response.json();
   } catch (error) {
     console.error(error);
